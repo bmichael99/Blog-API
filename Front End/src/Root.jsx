7b2d078e05f6a3c78e21a80d122722662e8d6a1a
Michael <michaelbrandmeyer99@gmail.com>
2025-07-07 15:52:59 -0700

    added AuthProvider here, I think this can be removed

--- Front End/src/Root.jsx
+++ Front End/src/Root.jsx
@@ -2,14 +2,17 @@
 import { useState } from "react";
 import App from "./App";
 import { Outlet } from "react-router-dom";
+import AuthProvider from "./context/XAuthProvider";
 
 function Root() {
   //const [state, setState] = useState([]);
 
   return (
+    <AuthProvider>
     <div>
       <Outlet context={{}} />
     </div>
+    </AuthProvider>
   );
 }
 
