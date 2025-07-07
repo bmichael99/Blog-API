79bb6cb02e3b58045af6c117f18bef54733abe3f
Michael <michaelbrandmeyer99@gmail.com>
2025-07-07 15:51:02 -0700

    added log out route

--- Back End/routes/authRouter.js
+++ Back End/routes/authRouter.js
@@ -6,6 +6,7 @@ const { isAuth } = require("../controllers/authMiddleware");
 
 authRouter.post("/auth/register", authController.registerUserPost);
 authRouter.post("/auth/login", authController.logInUserPost);
+authRouter.get("/auth/log-out", authController.handleLogout);
 //authRouter.get("/auth/me", requireAuth, authController.getCurrentUser);
 
 module.exports = authRouter;
