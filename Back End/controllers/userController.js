84d45b931149ce2af477e11430eef6368475d5a5
Michael <michaelbrandmeyer99@gmail.com>
2025-07-07 15:48:59 -0700

    removed creatUser

--- Back End/controllers/userController.js
+++ Back End/controllers/userController.js
@@ -7,19 +7,11 @@ require('dotenv').config()
 
 exports.getAllUsers = async (req,res) => {
   res.status(200).json({success: true, msg: "you are authorized!"});
+  //console.log("yuh");
 };
 
 exports.createUser = async (req,res) => {
-  try{
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = await db.createUser(req.body.first_name, req.body.username,hashedPassword);
-    
-    const jwt = utils.issueJWT(user);
-    res.json({success: true, user: user, token: jwt.token, expresIn: jwt.expires});
-  }
-  catch(err){
-    return next(err);
-  }
+
 };
 
 exports.updateUser = async (req,res) => {
