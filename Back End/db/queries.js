3bcfebd82c7553451402791b0206d5dd700edc98
Michael <michaelbrandmeyer99@gmail.com>
2025-07-07 15:49:30 -0700

    added GetUserById, getUserByRefreshToken, updateUserRefreshToken, updateUserDeleteRefreshToken

--- Back End/db/queries.js
+++ Back End/db/queries.js
@@ -1,18 +1,56 @@
 const prisma = require("./prisma");
 
-exports.createUser = async (first_name,username,password) => {
-  return await prisma.user.create({
-    data: {
-      first_name: first_name,
-      username: username,
-      password: password,
-    }
-  });
+exports.createUser = async (firstName,username,password) => {
+  try{
+    return await prisma.user.create({
+      data: {
+        firstName: firstName,
+        username: username,
+        password: password,
+      }
+    });
+  } catch(err){
+    throw err;
+  }
 }
 
 exports.getUserByUsername = async (username) => {
   const user = await prisma.user.findUnique({
     where: {username: username}
+  });
+
+  return user;
+}
+
+exports.getUserById = async (userId) => {
+  const user = prisma.user.findUnique({
+    where: {id: userId}
+  });
+
+  return user;
+}
+
+exports.getUserByRefreshToken = async (token) => {
+  const user = prisma.user.findFirst({
+    where: {refreshToken: token}
+  });
+
+  return user;
+}
+
+exports.updateUserRefreshToken = async (token, userId) => {
+  const user = await prisma.user.update({
+    where: {id: userId},
+    data: {refreshToken: token}
+  });
+
+  return user;
+}
+
+exports.updateUserDeleteRefreshToken = async (userId) => {
+  const user = await prisma.user.update({
+    where: {id: userId},
+    data: {refreshToken: null}
   });
 
   return user;
