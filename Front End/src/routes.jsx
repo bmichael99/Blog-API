a13fdd062ecbae4720cfc8bb85c70894f084ead5
Michael <michaelbrandmeyer99@gmail.com>
2025-07-07 15:53:17 -0700

    modified routes to no longer have all routes as children of the root

--- Front End/src/routes.jsx
+++ Front End/src/routes.jsx
@@ -1,21 +1,30 @@
 import Root from "./Root";
 import App from "./App";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+import Users from "./Users";
 import PostDirectory from "./PostDirectory";
 
 const routes = [
   {
     path: "/",
-    element: <Root />,
-    children: [
-      {
-        index: true, // same as path: "/"
-        element: <App />,
-      },
-      {
-        path: "/posts", // same as path: "/"
-        element: <PostDirectory />,
-      },
-    ],
+    element: <App />,
+  },
+  {
+    path: "/posts",
+    element: <PostDirectory />,
+  },
+  {
+    path: "/log-in",
+    element: <LogIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "/users",
+    element: <Users />,
   },
 ];
 
